f578f74daf9ae6324f1ec1a89d75ffb11b56b19c
geraldo <gerald-f@hotmail.fr>
2025-05-28 18:17:32 +0200

    Refactor code structure for improved readability and maintainability

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,19 +3,6 @@ import { ArrowLeft, LogOut } from "lucide-react";
 export const ArrowLeftIcon = ArrowLeft;
 export const LogOutIcon = LogOut;
 
-export const LogoIcon = (props) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    x="0px"
-    y="opx"
-    width={"100"}
-    height={"100"}
-    viewBox="0 0 32 32"
-    {...props}
-  >
-    <path
-      d="M 16 0 C 7.164062 0 0 7.164062 0 16 C 0 24.835938 7.164062 32 16 32 C 24.835938 32 32 24.835938 32 16 C 32 7.164062 24.835938 0 16 0 z M 16.5 4 L 15.5 4 L 15.5 12 L 8.5 12 L 8.5 14 L 15.5 14 L 15.5 22 L 16.5 22 L 16.5 14 L 23.5 14 L 23.5 12 L 16.5 12 z"
-      fill="#000000"
-    ></path>
-  </svg>
+export const LogoIcon = () => (
+  <img src="/logo.svg" alt="Pizza Planet Logo" width={76} height={76} />
 );
