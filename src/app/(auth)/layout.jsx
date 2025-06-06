708d8facd93d8681bb30877980a6981a39b640f2
geraldo <gerald-f@hotmail.fr>
2025-06-06 19:09:51 +0200

    fix: reorder imports in layout files for consistency
    - Moved import statements in layout.jsx and (auth)/layout.jsx for better organization.
    - Removed unused import in Navbar.jsx to clean up the code.

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -1,6 +1,6 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import Navbar from "@/components/navbar/Navbar";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function layout({ children }) {
   return (
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import Footer from "@/components/Footer";
+
 import { Toaster } from "sonner";
 import "./globals.css";
 
