271236917fe7d57abbea15b1226e7d9ea71e29ff
geraldo <gerald-f@hotmail.fr>
2025-06-17 17:46:50 +0200

    refactor: move session handling to UserDashboard component
    - Removed session management and sign-out functionality from the main page.
    - Integrated UserDashboard component to encapsulate user-specific dashboard features.
    - Enhanced UserDashboard with loading state and session validation for improved user experience.

--- src/app/mon-compte/page.jsx
+++ src/app/mon-compte/page.jsx
@@ -1,36 +1,9 @@
-"use client";
-import { useSession, signOut } from "@/lib/auth/auth-client";
+import UserDashboard from "@/components/dashboard/UserDashboard";
 
 export default function Page() {
-  const { data: session, isPending } = useSession();
-
-  if (isPending) {
-    return <div>Chargement...</div>;
-  }
-
-  if (!session) {
-    return <div>Vous devez être connecté pour accéder à cette page</div>;
-  }
-
-  const handleSignOut = async () => {
-    await signOut();
-    window.location.href = "/";
-  };
-
   return (
     <>
-      <div className="max-w-md mx-auto mt-8 p-6 bg-white rounded-lg shadow-md">
-        <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
-        <p>Bienvenue, {session.user.name}!</p>
-        <p>Email: {session.user.email}</p>
-
-        <button
-          onClick={handleSignOut}
-          className="mt-4 bg-red-500 text-white p-2 rounded-md hover:bg-red-600"
-        >
-          Se déconnecter
-        </button>
-      </div>
+      <UserDashboard />
     </>
   );
 }
--- src/components/dashboard/UserDashboard.jsx
+++ src/components/dashboard/UserDashboard.jsx
@@ -1,5 +1,36 @@
-import { Card } from "../ui/card";
+"use client";
+import { useSession, signOut } from "@/lib/auth/auth-client";
 
 export default function UserDashboard() {
-  return <Card>UserDashboard</Card>;
+  const { data: session, isPending } = useSession();
+
+  if (isPending) {
+    return <div>Chargement...</div>;
+  }
+
+  if (!session) {
+    return <div>Vous devez être connecté pour accéder à cette page</div>;
+  }
+
+  const handleSignOut = async () => {
+    await signOut();
+    window.location.href = "/";
+  };
+
+  return (
+    <>
+      <div className="max-w-md mx-auto mt-8 p-6 bg-white rounded-lg shadow-md">
+        <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
+        <p>Bienvenue, {session.user.name}!</p>
+        <p>Email: {session.user.email}</p>
+
+        <button
+          onClick={handleSignOut}
+          className="mt-4 bg-red-500 text-white p-2 rounded-md hover:bg-red-600"
+        >
+          Se déconnecter
+        </button>
+      </div>
+    </>
+  );
 }
